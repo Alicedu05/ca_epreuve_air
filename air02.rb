661810a20362ea892d5662f5ac27509548a77d2c
Alice Troussier <alicedu05@hotmail.fr>
2023-08-21 16:57:47 +0200

    J_avance_bien

--- air02.rb
+++ air02.rb
@@ -12,11 +12,24 @@
 #Afficher error et quitter le programme en cas de problèmes d’arguments.
 
 #FONCTION
+def concat(to_join, glue)
+	new_string = ""
+	new_string << to_join.join(glue)
+	
+	return new_string
+end
 
 #GESTION D'ERREUR
+abort("error") if ARGV.size < 2
+abort("error") if ARGV.empty?
+#abort("error") if ARGV.join !~ /[a-zA-Z]/ # concatene tous les mot de la chaine pour verifier si il n'y à que des lettres passées en arguments 
 
 #PARSING
+ string_to_join =ARGV[0..-2]
+ scotch = ARGV[-1]
 
 #RESOLUTION
+result = concat(string_to_join, scotch)
 
 #AFFICHAGE
+puts result.inspect
