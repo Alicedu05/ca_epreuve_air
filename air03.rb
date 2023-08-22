2e89c42c5e78bd6e771a8f12a18cf74093b78bab
Alice Troussier <alicedu05@hotmail.fr>
2023-08-22 10:03:50 +0200

    happe_easy

--- air03.rb
+++ air03.rb
@@ -9,11 +9,37 @@
 #Afficher error et quitter le programme en cas de problèmes d’arguments.
 
 #FONCTION
+def to_compare(list_elements)
+    odd = ""
+   
+    list_elements.each do |element|
+        cpt = 0
+
+        list_elements.each do |other_element|
+
+            if element.match?(other_element) 
+                cpt += 1
+            end 
+        end
+
+        if cpt == 1
+            odd << element
+        end
+
+    end
+    return odd
+
+end
 
 #GESTION D'ERREUR
+abort("error") if ARGV.size < 2
+abort("error") if ARGV.empty?
 
 #PARSING
+list = ARGV
 
 #RESOLUTION
+result = to_compare(list)
 
 #AFFICHAGE
+puts result
