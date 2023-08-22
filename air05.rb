a97bb39b0af3bc724e5fb788a994d9b2488c58d9
Alice Troussier <alicedu05@hotmail.fr>
2023-08-22 17:58:37 +0200

    simple

--- air05.rb
+++ air05.rb
@@ -10,11 +10,38 @@
 #Afficher error et quitter le programme en cas de problèmes d’arguments.
 
 #FONCTION
+def calculat(liste_of_integer, operateur)
+    symbol = operateur[0] 
+    chiffre = operateur[1].to_i 
+    liste_solution = []
+
+    liste_of_integer.each do |number|
+
+        if symbol == "-" # Si l'opérateur est "-"
+            solution = number - chiffre
+        else
+            solution = number + chiffre
+        end
+
+        liste_solution << solution
+    end
+
+return liste_solution
+end
 
 #GESTION D'ERREUR
+abort("error") if ARGV.size < 2
+abort("error") if ARGV.empty?
+if ARGV.any? { |arg| arg =~ /[a-zA-Z]/ }
+  abort("error")
+end
 
 #PARSING
+list_of_integer = ARGV[0..-2].map(&:to_i)  
+operateur = ARGV[-1] # "+2"
 
 #RESOLUTION
+result = calculat(list_of_integer, operateur)
 
 #AFFICHAGE
+print result.join(" ")
