b141ad9bf86952d4ef23c7061051e3601372a8ce
Alice Troussier <alicedu05@hotmail.fr>
2023-08-24 12:34:35 +0200

    trop_bien

--- air06.rb
+++ air06.rb
@@ -14,11 +14,40 @@
 #Afficher error et quitter le programme en cas de problèmes d’arguments.
 
 #FONCTION
+def compare(string_list, model)
+	copie_string_list = string_list.dup
+	new_array = []
+
+	copie_string_list.each do |word|
+		identical = false
+
+		word.each_char do |char|
+			i = 0
+			while i < word.size
+				if char == model
+					identical = true
+					break
+				end
+				i += 1
+			end
+		end
+		if identical == false
+			new_array << word
+		end
+	end
+	return new_array
+end
 
 #GESTION D'ERREUR
+abort("error") if ARGV.size < 2
+abort("error") if ARGV.empty?
 
 #PARSING
+list = ARGV[0..-2] 
+model_string = ARGV[-1]
 
 #RESOLUTION
+result = compare(list, model_string)
 
 #AFFICHAGE
+print result.join(" ")
