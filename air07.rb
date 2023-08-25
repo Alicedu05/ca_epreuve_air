13e40b530940786469459948b0e5c5c973a68623
Alice Troussier <alicedu05@hotmail.fr>
2023-08-25 17:32:25 +0200

    balade

--- air07.rb
+++ air07.rb
@@ -14,11 +14,39 @@
 #Afficher error et quitter le programme en cas de problèmes d’arguments.
 
 #FONCTION
+def insert(int_list,num)
+	copy_int_list = int_list.dup
+	copy_int_list << num
+	
+	for i in (copy_int_list.size - 1).downto(0) # bubble_sort
+        tableau_trie = true 
+
+        for j in 0..i-1 do 
+            if copy_int_list[j+1] < copy_int_list[j]
+                temp = copy_int_list[j]
+                copy_int_list[j] = copy_int_list[j+1]
+                copy_int_list[j+1] = temp
+
+                tableau_trie = false
+            end
+        end
+        break if tableau_trie
+    end
+
+	return copy_int_list
+end
 
 #GESTION D'ERREUR
+abort("error") if ARGV.size < 3
+abort("error") if ARGV.empty?
+abort("error") if ARGV.join =~ /[a-zA-Z]/ # concatene tous 
 
 #PARSING
+number_list = ARGV[0..-2].map(&:to_i) 
+to_class = ARGV[-1].to_i 
 
 #RESOLUTION
+complete_list =  insert(number_list,to_class)
 
 #AFFICHAGE
+print complete_list.join(" ")
