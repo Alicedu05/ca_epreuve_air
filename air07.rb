7140a17a2cc3040aabff7e7c6f0e8d15409d7d48
Alice Troussier <alicedu05@hotmail.fr>
2023-08-28 15:45:37 +0200

    modif_de_la_fonction

--- air07.rb
+++ air07.rb
@@ -16,24 +16,29 @@
 #FONCTION
 def insert(int_list,num)
 	copy_int_list = int_list.dup
-	copy_int_list << num
-	
-	for i in (copy_int_list.size - 1).downto(0) # bubble_sort
-        tableau_trie = true 
+    size = copy_int_list.size
+	new_array = []
+    i = 0
 
-        for j in 0..i-1 do 
-            if copy_int_list[j+1] < copy_int_list[j]
-                temp = copy_int_list[j]
-                copy_int_list[j] = copy_int_list[j+1]
-                copy_int_list[j+1] = temp
-
-                tableau_trie = false
-            end
-        end
-        break if tableau_trie
+    if num <= copy_int_list[0] 
+        new_array << num 
     end
 
-	return copy_int_list
+    while i < copy_int_list.size
+        if copy_int_list[i] < num && num < copy_int_list[i+1]
+            new_array << copy_int_list[i]
+            new_array << num
+
+        elsif num >= copy_int_list[-1]
+                new_array << copy_int_list
+                new_array << num
+                break
+        else
+            new_array << copy_int_list[i]
+        end
+            i += 1
+    end
+	return new_array
 end
 
 #GESTION D'ERREUR
@@ -50,3 +55,31 @@ complete_list =  insert(number_list,to_class)
 
 #AFFICHAGE
 print complete_list.join(" ")
+
+
+#nombre_a_ajouter = 33
+#index_insertion = liste.index { |nombre| nombre > nombre_a_ajouter } || liste.size
+#liste.insert(index_insertion, nombre_a_ajouter)
+#puts liste
+
+#def insert(int_list,num)
+#	copy_int_list = int_list.dup
+#	copy_int_list << num
+	
+#	for i in (copy_int_list.size - 1).downto(0) # bubble_sort
+ #       tableau_trie = true 
+
+  #      for j in 0..i-1 do 
+   #         if copy_int_list[j+1] < copy_int_list[j]
+    #            temp = copy_int_list[j]
+     #           copy_int_list[j] = copy_int_list[j+1]
+      #          copy_int_list[j+1] = temp
+
+       #        tableau_trie = false
+        #end
+        #end
+        #break if tableau_trie
+    #end
+
+	#return copy_int_list
+#end
