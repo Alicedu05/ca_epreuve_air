b05c681b0e40e6c41e6bf37260d32e162ef6d156
Alice Troussier <alicedu05@hotmail.fr>
2023-08-30 13:54:52 +0200

    exo_simpatique

--- air08.rb
+++ air08.rb
@@ -13,10 +13,65 @@
 
 #FONCTION
 
+def cut(text_to_cut, string_cuter)
+	first_array = []
+	second_array= []
+	index_cuter = text_to_cut.find_index(string_cuter)
+
+	for i in 0..index_cuter-1
+		text_to_cut[i]
+		first_array << text_to_cut[i]
+	end
+
+	for j in index_cuter+1...text_to_cut.size
+		text_to_cut[j]
+		second_array << text_to_cut[j]
+	end
+	
+return liste = [first_array, second_array]
+end
+
+def insert(num_liste1,num_liste2)
+	list1_copy = num_liste1.dup
+	list2_copy = num_liste2.dup
+    max_size = [list1_copy.size, list2_copy.size].max
+	new_array = []
+    
+	max_size.times do |index|
+		if list1_copy[index] == nil || list2_copy[index] == nil
+			remaining_elements = []
+			remaining_elements.concat(list1_copy[index..-1]) if list2_copy[index].nil?
+			remaining_elements.concat(list2_copy[index..-1]) if list1_copy[index].nil?
+			
+			new_array.concat(remaining_elements)
+			break
+
+		end
+
+		if list1_copy[index] < list2_copy[index]
+			new_array << list1_copy[index] << list2_copy[index]
+		else
+			new_array << list2_copy[index] << list1_copy[index]
+		end
+
+	end
+	return new_array
+end
+
 #GESTION D'ERREUR
+abort("error") if ARGV.size < 3
+abort("error") if ARGV.empty?
+abort("error") if !ARGV.include?("fusion")
+
+#abort("error") if ARGV.join =~ /[a-zA-Z]/ # concatene tous 
 
 #PARSING
+to_cut = ARGV[0..-1]#.map(&:to_s).join(' ') # transformer array en string separé d'un espace
+cuter = "fusion"
 
 #RESOLUTION
+number_array = cut(to_cut, cuter)
+result = insert(number_array[0],number_array[1])
 
 #AFFICHAGE
+print result
