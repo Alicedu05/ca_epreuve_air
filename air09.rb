9177d88e8665ab88910e9318483372c4ac0686df
Alice Troussier <alicedu05@hotmail.fr>
2023-08-31 13:04:22 +0200

    new_algo_plus_lisible

--- air09.rb
+++ air09.rb
@@ -12,11 +12,43 @@
 #Afficher error et quitter le programme en cas de problèmes d’arguments.
 
 #FONCTION
+def turn(array)
+	array_copy = array.dup
+	new_array = [] 
+	i = 1
+
+	while i < array_copy.size
+		new_array << array_copy[i]
+		i+=1
+	end
+
+	new_array << array_copy[0]
+	return new_array 
+end
 
 #GESTION D'ERREUR
+abort("error") if ARGV.size < 2
+abort("error") if ARGV.empty?
+
 
 #PARSING
+array = ARGV
 
 #RESOLUTION
+result = turn(array)
+#result = ma_rotation(array)
+
 
 #AFFICHAGE
+puts result.join(", ")
+
+#def ma_rotation(array) solution trouvé en premier mais qui me parait compliqué a lire
+#	array_copy = array.dup
+#	old_position = 0
+#	new_position = -1
+
+#	to_mouve = array_copy.delete_at(old_position)
+#	array_copy.insert(new_position, to_mouve)
+	
+#	return array_copy
+#end
