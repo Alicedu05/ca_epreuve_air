7ea9cc30417f7ebcbf6448a26d173cfeccc2c40d
Alice Troussier <alicedu05@hotmail.fr>
2023-08-31 18:41:49 +0200

    ok

--- air10.rb
+++ air10.rb
@@ -9,11 +9,21 @@
 #Afficher error et quitter le programme en cas de problèmes d’arguments ou de fichier inaccessible.
 
 #FONCTION
+def reader(file)
+    File.read(file)
+end
 
 #GESTION D'ERREUR
+abort("error") if ARGV.size < 1
+abort("error") if ARGV.empty?
+abort("error") if File.extname(ARGV[0]) != ".txt"
+abort("error") unless File.exist?(ARGV[0])
 
 #PARSING
+argument = ARGV[0]
 
 #RESOLUTION
+read_txt = reader(argument)
 
 #AFFICHAGE
+puts read_txt
