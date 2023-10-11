bf627883b98181468262290e744d4571e8bfd76e
Alicedu05 <alicedu05@hotmail.fr>
2023-10-11 12:19:27 +0200

    fin de l'epreuve

--- air14.rb
+++ air14.rb
@@ -1,25 +1,10 @@
-#Meta exercice
+#Air : ok
 
-#Créez un programme qui vérifie si les exercices de votre épreuve de l’air sont présents dans le répertoire et qu’ils fonctionnent (sauf celui là). 
-#Créez au moins un test pour chaque exercice
+#Créez un programme qui célèbre votre victoire.
 #Exemples d’utilisation :
 #$> python exo.py
-#air00 (1/3) : success
-#air00 (2/3) : success
-#air00 (3/3) : success
-#air01 (1/2) : success
-#air01 (2/2) : failure
-#air02 (1/1) : success
-#... 
-#Total success: (56/62)
-#Bonus : trouvez le moyen d’utiliser du vert et du rouge pour rendre réussites et échecs plus visibles.
+#J’ai terminé l’Épreuve de l’Air et c’était [].
+#$>
+#Note : [] est à remplacer par un adjectif de votre choix (facile ?)
 
-#FONCTION
-
-#GESTION D'ERREUR
-
-#PARSING
-
-#RESOLUTION
-
-#AFFICHAGE
+print "Je viens de terminer l'épreuve de l'air. Pour fêter cette avancée je vais changer de garde robe ;-p "
